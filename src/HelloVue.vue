9e1b342050a6c7ff47c2a91d584c3dd6cddf8ea5
mauricio <mauricio.vera.r7@gmail.com>
2021-09-16 13:20:32 -0300

    removido node_modules

--- src/HelloVue.vue
+++ src/HelloVue.vue
@@ -1,35 +1,19 @@
 <template>
     <div class="container">
-        <div class="hellovue">Crear una nueva tarea
-
-        </div>
             <div class="tarea">
                 Tarea
                 <input type="text" v-model="texto" placeholder="ingresa nueva tarea"/>
-                <button @click="guardar">Crear</button>
-
-                
-                    <p class="lista">Lista</p>
+                <button @click="guardar">Añade</button >
             </div>
 
             <div class="listaOk" v-if="listaOk">
                 <ul>
                     <li v-for="(item , index) in lista" :key="index">
-                        <span>{{ index }}: {{ item }}</span>                  
+                        <span>{{ index }}: {{ item }}</span>
+                        <button @click="lista.splice(index,1)">eliminar</button> 
+                        <!--$emit se puede enviar directo desde el boton-->    
                     </li>
                 </ul>
-            </div>
-
-            <div>
-                cuenta regresiva
-                <button @click="b1">1</button>
-                <button @click="b2">2</button>
-                <button @click="b3">3</button>
-            </div>
-
-            <div v-if="mostrar">
-                        {{ tiempo }}
-                
             </div>
     </div>
 </template>
@@ -39,11 +23,11 @@
         name: 'HelloVue',
         data() {
             return {
-                listaOk: false,
+                listaOk: true,
                 lista: [],
                 tiempo:'',
-                texto : '',
-                mostrar: false              
+                texto : '', 
+                eliminado:0          
             };
         },
         methods: {
@@ -52,56 +36,7 @@
                 this.texto = ""
                 this.listaOk = "!this.listaOk"                    
             },
-            b1() {
-                this.mostrar = "!this.mostrar" 
-                this.tiempo = 1
-                //tiempo = setInterval(1000);  
-                //this.tiempo = setInterval(20000); 
-                //this.tiempo = setInterval(function(){ 'this.tiempo' }, 3000);
-                
-                //this.tiempo = setTimeout(this.tiempo, 3000);
-                //setTimeout(() => {this.tiempo}, 5000);
-                setTimeout(() => {
-                this.tiempo--
-                }, 1000);
-            },
-            b2() {
-                this.mostrar = "!this.mostrar" 
-                this.tiempo = 2
-                //tiempo = setInterval(1000);  
-                //this.tiempo = setInterval(20000); 
-                //this.tiempo = setInterval(function(){ 'this.tiempo' }, 3000);
-                
-                //this.tiempo = setTimeout(this.tiempo, 3000);
-                //setTimeout(() => {this.tiempo}, 5000);
-                setTimeout(() => {
-                    setTimeout(() => {
-                this.tiempo--
-                }, 2000);
-                this.tiempo--
-                }, 1000);
-            },
-            b3() {
-                this.mostrar = "!this.mostrar" 
-                this.tiempo = 3
-                //tiempo = setInterval(1000);  
-                //this.tiempo = setInterval(20000); 
-                //this.tiempo = setInterval(function(){ 'this.tiempo' }, 3000);
-                
-                //this.tiempo = setTimeout(this.tiempo, 3000);
-                //setTimeout(() => {this.tiempo}, 5000);
-                setTimeout(() => {
-                    setTimeout(() => {
-                        setTimeout(() => {
-                this.tiempo--
-                }, 3000);
-                this.tiempo--
-                }, 2000);
-                this.tiempo--
-                }, 1000);
-            },
-        }
-
+        },
     };
 </script>
 
@@ -113,6 +48,7 @@
         color: gray;
         font-weight: bold;
         font-size: 50px;
+
     }
     .tarea {
         color: black;
@@ -120,6 +56,9 @@
         font-size: 20px;
         margin-top: 20px;
         margin-left: 50px;
+        color:blue;
+
+
 
     }
     .lista {
@@ -127,10 +66,15 @@
         font-weight: bold;
         font-size: 50px;
         margin-left: 100px;
+
     }
         .listaOk {
         color: black;
         font-weight: bold;
         margin-left: 100px;
+
+    }
+    button {
+        border-radius: 12%;
     }
 </style>
---
+++ src/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">        
+        <HelloVue></HelloVue>        
+    </div>
+</template>
+
+<script>
+    import HelloVue from './HelloVue.vue';
+
+    export default {
+        name: "App",
+        components: {
+            HelloVue
+        },
+
+        data() {
+            return {
+            }
+        }
+    };
+</script>
